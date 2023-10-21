4ffe93e71c07d43e2fee48d0aaaa8cb07bcf5aff
Dadush0 <davide.dagostini.2021@mumail.ie>
2023-10-21 14:20:07 +0100

    Random examples for the LineupWidget.

--- lib/presentation/widgets/LineupWidget.dart
+++ lib/presentation/widgets/LineupWidget.dart
@@ -5,86 +5,202 @@ class LineupWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DataTable(
-      columns: const <DataColumn>[
-        DataColumn(
-          label: Expanded(
-            child: Text(
-              'Date',
-              style: TextStyle(fontStyle: FontStyle.italic),
+    return SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: DataTable(
+          columns: const <DataColumn>[
+            DataColumn(
+              label: Expanded(
+                child: Text(
+                  'Date',
+                  style: TextStyle(fontStyle: FontStyle.italic),
+                ),
+              ),
             ),
-          ),
-        ),
-        DataColumn(
-          label: Expanded(
-            child: Text(
-              'Time',
-              style: TextStyle(fontStyle: FontStyle.italic),
+            DataColumn(
+              label: Expanded(
+                child: Text(
+                  'Time',
+                  style: TextStyle(fontStyle: FontStyle.italic),
+                ),
+              ),
             ),
-          ),
-        ),
-        DataColumn(
-          label: Expanded(
-            child: Text(
-              'Team',
-              style: TextStyle(fontStyle: FontStyle.italic),
+            DataColumn(
+              label: Expanded(
+                child: Text(
+                  'Team',
+                  style: TextStyle(fontStyle: FontStyle.italic),
+                ),
+              ),
             ),
-          ),
-        ),
-        DataColumn(
-          label: Expanded(
-            child: Text(
-              'Where TF',
-              style: TextStyle(fontStyle: FontStyle.italic),
+            DataColumn(
+              label: Expanded(
+                child: Text(
+                  'Where TF',
+                  style: TextStyle(fontStyle: FontStyle.italic),
+                ),
+              ),
             ),
-          ),
-        ),
-        DataColumn(
-          label: Expanded(
-            child: Text(
-              'Opponent',
-              style: TextStyle(fontStyle: FontStyle.italic),
+            DataColumn(
+              label: Expanded(
+                child: Text(
+                  'Opponent',
+                  style: TextStyle(fontStyle: FontStyle.italic),
+                ),
+              ),
             ),
-          ),
-        ),
-        DataColumn(
-          label: Expanded(
-            child: Text(
-              'Players',
-              style: TextStyle(fontStyle: FontStyle.italic),
-              overflow: TextOverflow.visible,
+            DataColumn(
+              label: Expanded(
+                child: Text(
+                  'Players',
+                  style: TextStyle(fontStyle: FontStyle.italic),
+                  overflow: TextOverflow.visible,
+                ),
+              ),
             ),
-          ),
-        ),
-        DataColumn(
-          label: Expanded(
-            child: Text(
-              'Cake and Pints',
-              style: TextStyle(fontStyle: FontStyle.italic),
+            DataColumn(
+              label: Expanded(
+                child: Text(
+                  'Cake and Pints',
+                  style: TextStyle(fontStyle: FontStyle.italic),
+                ),
+              ),
             ),
-          ),
-        ),
-        DataColumn(
-          label: Expanded(
-            child: Text(
-              'Supervisor',
-              style: TextStyle(fontStyle: FontStyle.italic),
+            DataColumn(
+              label: Expanded(
+                child: Text(
+                  'Supervisor',
+                  style: TextStyle(fontStyle: FontStyle.italic),
+                ),
+              ),
             ),
-          ),
+          ],
+          rows: const <DataRow>[
+            DataRow(cells: <DataCell>[
+              DataCell(Text('21/10/2023')),
+              DataCell(Text('13:00')),
+              DataCell(Text('Prpject')),
+              DataCell(Text('Home')),
+              DataCell(Text('Amberg')),
+              DataCell(Text('DAgostini \n writz \n fucker \n djkqg')),
+              DataCell(Text('Writz')),
+              DataCell(Text('Mineself')),
+            ]),
+            DataRow(cells: <DataCell>[
+              DataCell(Text('21/10/2023')),
+              DataCell(Text('13:00')),
+              DataCell(Text('Prpject')),
+              DataCell(Text('Home')),
+              DataCell(Text('Amberg')),
+              DataCell(Text('DAgostini \n writz')),
+              DataCell(Text('Writz')),
+              DataCell(Text('Mineself')),
+            ]),
+            DataRow(cells: <DataCell>[
+              DataCell(Text('21/10/2023')),
+              DataCell(Text('13:00')),
+              DataCell(Text('Prpject')),
+              DataCell(Text('Home')),
+              DataCell(Text('Amberg')),
+              DataCell(Text('DAgostini \n writz \n fucker \n djkqg')),
+              DataCell(Text('Writz')),
+              DataCell(Text('Mineself')),
+            ]),
+            DataRow(cells: <DataCell>[
+              DataCell(Text('21/10/2023')),
+              DataCell(Text('13:00')),
+              DataCell(Text('Prpject')),
+              DataCell(Text('Home')),
+              DataCell(Text('Amberg')),
+              DataCell(Text('DAgostini \n writz \n fucker \n djkqg')),
+              DataCell(Text('Writz')),
+              DataCell(Text('Mineself')),
+            ]),
+            DataRow(cells: <DataCell>[
+              DataCell(Text('21/10/2023')),
+              DataCell(Text('13:00')),
+              DataCell(Text('Prpject')),
+              DataCell(Text('Home')),
+              DataCell(Text('Amberg')),
+              DataCell(Text('DAgostini \n writz')),
+              DataCell(Text('Writz')),
+              DataCell(Text('Mineself')),
+            ]),
+            DataRow(cells: <DataCell>[
+              DataCell(Text('21/10/2023')),
+              DataCell(Text('13:00')),
+              DataCell(Text('Prpject')),
+              DataCell(Text('Home')),
+              DataCell(Text('Amberg')),
+              DataCell(Text('DAgostini \n writz \n fucker \n djkqg')),
+              DataCell(Text('Writz')),
+              DataCell(Text('Mineself')),
+            ]),
+            DataRow(cells: <DataCell>[
+              DataCell(Text('21/10/2023')),
+              DataCell(Text('13:00')),
+              DataCell(Text('Prpject')),
+              DataCell(Text('Home')),
+              DataCell(Text('Amberg')),
+              DataCell(Text('DAgostini \n writz \n fucker \n djkqg')),
+              DataCell(Text('Writz')),
+              DataCell(Text('Mineself')),
+            ]),
+            DataRow(cells: <DataCell>[
+              DataCell(Text('21/10/2023')),
+              DataCell(Text('13:00')),
+              DataCell(Text('Prpject')),
+              DataCell(Text('Home')),
+              DataCell(Text('Amberg')),
+              DataCell(Text('DAgostini \n writz')),
+              DataCell(Text('Writz')),
+              DataCell(Text('Mineself')),
+            ]),
+            DataRow(cells: <DataCell>[
+              DataCell(Text('21/10/2023')),
+              DataCell(Text('13:00')),
+              DataCell(Text('Prpject')),
+              DataCell(Text('Home')),
+              DataCell(Text('Amberg')),
+              DataCell(Text('DAgostini \n writz \n fucker \n djkqg')),
+              DataCell(Text('Writz')),
+              DataCell(Text('Mineself')),
+            ]),
+            DataRow(cells: <DataCell>[
+              DataCell(Text('21/10/2023')),
+              DataCell(Text('13:00')),
+              DataCell(Text('Prpject')),
+              DataCell(Text('Home')),
+              DataCell(Text('Amberg')),
+              DataCell(Text('DAgostini \n writz \n fucker \n djkqg')),
+              DataCell(Text('Writz')),
+              DataCell(Text('Mineself')),
+            ]),
+            DataRow(cells: <DataCell>[
+              DataCell(Text('21/10/2023')),
+              DataCell(Text('13:00')),
+              DataCell(Text('Prpject')),
+              DataCell(Text('Home')),
+              DataCell(Text('Amberg')),
+              DataCell(Text('DAgostini \n writz')),
+              DataCell(Text('Writz')),
+              DataCell(Text('Mineself')),
+            ]),
+            DataRow(cells: <DataCell>[
+              DataCell(Text('21/10/2023')),
+              DataCell(Text('13:00')),
+              DataCell(Text('Prpject')),
+              DataCell(Text('Home')),
+              DataCell(Text('Amberg')),
+              DataCell(Text('DAgostini \n writz \n fucker \n djkqg')),
+              DataCell(Text('Writz')),
+              DataCell(Text('Mineself')),
+            ])
+          ],
         ),
-      ],
-      rows: const <DataRow>[
-        DataRow(cells: <DataCell>[
-          DataCell(Text('21/10/2023')),
-          DataCell(Text('13:00')),
-          DataCell(Text('Prpject')),
-          DataCell(Text('Home')),
-          DataCell(Text('Amberg')),
-          DataCell(Text('DAgostini \n writz \n fucker \n djkqg')),
-          DataCell(Text('Writz')),
-          DataCell(Text('Mineself')),
-        ])
-      ],
+      ),
     );
   }
 }
